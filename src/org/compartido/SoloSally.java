1b4c1cab5fd50a021537a4e92ffed59a7f112166
Raul Galan <raulgalan2001@gmail.com>
2021-04-27 09:38:24 +0200

    Segundo commit de Sally

--- src/org/compartido/SoloSally.java
+++ src/org/compartido/SoloSally.java
@@ -1,8 +1,8 @@
 package org.compartido;
 
-public class Harry {
+public class SoloSally {
 	public static void main(String[] args) {
-		System.out.println("HARRY");
+		System.out.println("SALLY");
 	}
 
 }
