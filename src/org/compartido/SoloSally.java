a00393941a6f131a6597669308cdd516f6c86534
Raul Galan <raulgalan2001@gmail.com>
2021-04-27 10:27:50 +0200

    Tercer commit de Sally

--- src/org/compartido/SoloSally.java
+++ src/org/compartido/SoloSally.java
@@ -4,5 +4,13 @@ public class SoloSally {
 	public static void main(String[] args) {
 		System.out.println("SALLY");
 	}
+	
+	private void sally() {
+		System.out.println("SALLY");
+	}	
+	
+	private void harry() {
+		System.out.println("HARRY");
+	}
 
 }
